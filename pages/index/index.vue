6824d8c57ac84b126adb01c8b61982a359bce8b5
leixiaopeng <2672947175@qq.com>
2020-11-11 12:05:14 +0800

    lxp

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,255 +3,344 @@
 		<!-- 头部 -->
 		<view class="top-box">
 			<view class="header">
-			  <span class='spansss'>
-			   <span class="icon iconfont" @click='tonews'>&#xe638;</span>
-			  </span>
-			  <span class='spansss'>
-			   <span class="icon iconfont" @click='tomenu'>&#xe610;</span>
-			  </span>
-			  <span class='spansss'>
-			   <span class="icon iconfont" @click='tomy'>&#xe61a;</span>
-			 </span>
+				<span class="spansss"><span class="icon iconfont" @click="tonews">&#xe638;</span></span>
+				<span class="spansss"><span class="icon iconfont" @click="tomenu">&#xe610;</span></span>
+				<span class="spansss"><span class="icon iconfont" @click="tomy">&#xe61a;</span></span>
 			</view>
-			<view class="name-box">
-				司机端
-			</view>
-			<view class="title-box">
-				Tian Tian I
-			</view>
+			<view class="name-box">司机端</view>
+			<view class="title-box">Tian Tian I</view>
 			<!-- 预留input框  -->
 			<view class="intput-box" @click="btns()">
-				<!-- <input type="text" v-model="shouvalue"  /> -->
+				{{targetName}}
 			</view>
 			<!-- 切换 -->
 			<view class="switch-box">
-				<view class="switch-item"  v-for='(item,index) in switchList' :key='index' @click="onSwitch(index)" :class="indexs==index?'switch-item-color':''" >
-					{{item.title}}
+				<view class="switch-item" v-for="(item, index) in switchList" :key="index" @click="onSwitch(index)" :class="indexs == index ? 'switch-item-color' : ''">
+					{{ item.title }}
 				</view>
 			</view>
 		</view>
 		<!-- 地图盒子 -->
 		<view class="map-box">
-			 <map style="width: 100vw; height: 51vh;" @tap='huo' :enable-traffic='true' :latitude="latitude" :longitude="longitude" :markers="covers" :polyline='polyline' >
-			  </map>
+			<map
+				v-if="indexs == 0"
+				style="width: 100vw; height: 51vh;"
+				:enable-traffic="true"
+				:latitude="latitude"
+				:longitude="longitude"
+				:markers="markers"
+				:polyline="polyline"
+				:show-compass='true'
+			>
+			<cover-view class="shishi-box" >
+					预计耗时{{duration}}小时
+					
+			</cover-view >
+			<cover-view class="shishi-boxa" >
+						全程 {{distance}} 
+			</cover-view >
+			
+			</map>
+			
 		</view>
-	
+		
+			<!-- <view  v-for="(item,index) in stepsList" :key='index'>
+			   {{index+1}}.{{item.instruction}}
+			</view> -->
+		<!-- </view> -->
 		<!-- 底部 -->
-		<view class="bootm-box">
-			<view class="b-r">
-				<span>首页</span>
-			</view>
-			<view>
-				<span>订单</span>
-			</view>
+		<view  class="bootm-box">
+			<view class="b-r"><span>首页</span></view>
+			<view><span>订单</span></view>
 		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				shouvalue:'',
-				indexs:0,
-				id:0, // 使用 marker点击事件 需要填写id
-				latitude:0,
-				longitude: 0,
-				covers: [],
-				polyline:[],
-				switchList:[
-					{
-						title:'实时'
-					},
-					{
-						title:'预约'
-					},
-					{
-						title:'附近司机'
-					},
-					{
-						title:'热力图'
-					}
-				]
-			}
-		},
-		onLoad(){
-			this.onGetLocation()
-		},
-		methods: {
-			huo(){
-				this.onGetLocation()
-			},
-			btns(){
-				this.onGetLocation()
-				let _this = this
-				uni.chooseLocation({
-				    success: function (res) {
-				        // console.log('位置名称：' + res.name);
-				        // console.log('详细地址：' + res.address);
-				        // console.log('纬度：' + res.latitude);
-				        // console.log('经度：' + res.longitude);
-						// let covers = JSON.parse(JSON.stringify(_this.covers))
-						let obj = {
-							latitude:res.latitude*1,
-							longitude:res.longitude*1,
-							iconPath: '../../static/touxiang.png'
+import amapFile from '../../static/amap-wx.js';
+export default {
+	data() {
+		return {
+			indexs: 0,
+			switchList: [
+				{
+					title: '实时'
+				},
+				{
+					title: '预约'
+				},
+				{
+					title: '附近司机'
+				},
+				{
+					title: '热力图'
+				}
+			],
+			longitude: 0,
+			latitude: 0,
+			
+			markers: [],
+			polyline: [],
+			stepsList:[],
+			targetName:'',
+			distance: '',
+			cost: '',
+			duration:''
+		};
+	},
+	onLoad() {
+		this.onGetLocation();
+	},
+	methods: {
+		btns() {
+			// 搜索目的地
+			this.onGetLocation();
+			let _this = this;
+			uni.chooseLocation({
+				success: function(res) {
+					// console.log('位置名称：' + res.name);
+					// console.log('详细地址：' + res.address);
+					// console.log('纬度：' + res.latitude);
+					// console.log('经度：' + res.longitude);
+					_this.targetName = res.name
+					let obj = {
+						longitude: res.longitude * 1,
+						latitude: res.latitude * 1,
+						iconPath: '../../static/touxiang.png'
+					};
+					_this.markers.push(obj);
+					// 获取路线
+					let myAmapFun = new amapFile.AMapWX({ key: 'd543ccedafcc74cd880a88bfbc0b2576' });
+					myAmapFun.getDrivingRoute({
+						origin: `${_this.longitude},${_this.latitude}`,
+						destination: `${res.longitude},${res.latitude}`,
+						success: function(data) {
+							console.log(data);
+							
+							var points = [];
+							if (data.paths && data.paths[0] && data.paths[0].steps) {
+								var steps = data.paths[0].steps;
+								for (var i = 0; i < steps.length; i++) {
+									var poLen = steps[i].polyline.split(';');
+									for (var j = 0; j < poLen.length; j++) {
+										points.push({
+											longitude: parseFloat(poLen[j].split(',')[0]),
+											latitude: parseFloat(poLen[j].split(',')[1])
+										});
+									}
+								}
+								
+								// _this.stepsList =data.paths[0].steps
+							}
+
+							_this.polyline = [
+								{
+									points: points,
+									color: '#ED1C24',
+									width: 10
+								}
+							];
+							// if(){
+								
+								_this.duration = Math.round(data.paths[0].duration * 10/60/60) / 10
+							// }
+							if (data.paths[0] && data.paths[0].distance) {
+								_this.distance = data.paths[0].distance + '米';
+								console.log(_this.distance);
+							}
+							if (data.taxi_cost) {
+								_this.cost = '打车约' + parseInt(data.taxi_cost) + '元';
+								console.log(_this.cost);
+							}
 						}
-						
-						_this.covers = [..._this.covers,obj];
-						_this.polyline=[{
-							points:_this.covers,
-							color: '#FF5051',
-							borderColor: '#FF5051',
-							borderWidth:10,
-							width:10,
-							arrowLine:true
-						}]
-				    }
-				});
-				
-			},
-			// 获取当前的地理位置
-			onGetLocation(){
-				let _this = this
-				uni.getLocation({
-				    type: 'wgs84',
-				    success: function (res) {
-						console.log(res)
-						_this.longitude = res.longitude
-						_this.latitude =res.latitude
-				        // console.log('当前位置的经度：' + res.longitude);
-				        // console.log('当前位置的纬度：' + res.latitude);
-						let obj = {
-							longitude:res.longitude*1,
-							latitude:res.latitude*1 ,
-							iconPath: '../../static/touxiang.png'
-						}
-						
-						_this. = [obj]
-						
-						
-				    }
-				});
-			},
+					});
+				}
+			});
+		},
+		// 获取当前的地理位置
+		onGetLocation() {
+			let _this = this;
+			uni.getLocation({
+			    type: 'gcj02',
+			    success: function (res) {
+			        console.log('当前位置的经度：' + res.longitude);
+			        console.log('当前位置的纬度：' + res.latitude);
+							_this.longitude = res.longitude;
+							_this.latitude = res.latitude;
+							let obj = {
+								longitude: res.longitude * 1,
+								latitude: res.latitude * 1,
+								iconPath: '../../static/touxiang.png'
+							};
+							_this.markers = [obj];
+			    }
+			});
+			// let myAmapFun = new amapFile.AMapWX({ key: 'd543ccedafcc74cd880a88bfbc0b2576' });
+			// myAmapFun.getRegeo({
+			// 	success: function(data) {
+			// 		let res = data[0];
+			// 		_this.longitude = res.longitude;
+			// 		_this.latitude = res.latitude;
+
+			// 		let obj = {
+			// 			longitude: res.longitude * 1,
+			// 			latitude: res.latitude * 1,
+			// 			iconPath: '../../static/touxiang.png'
+			// 		};
+			// 		_this.markers = [obj];
+			// 		//成功回调
+			// 	},
+			// 	fail: function(info) {
+			// 		//失败回调
+			// 		console.log(info);
+			// 	}
+			// });
+		},
 		// 切换
-		onSwitch(id){
-			this.indexs = id
+		onSwitch(id) {
+			this.indexs = id;
 		},
 		// 去消息
-		tonews(){
+		tonews() {
 			uni.navigateTo({
-			    url: '/pages/news/news'
+				url: '/pages/news/news'
 			});
 		},
 		// 去设置
-		tomenu(){
+		tomenu() {
 			uni.navigateTo({
-			    url: '/pages/menu/menu'
+				url: '/pages/menu/menu'
 			});
 		},
 		// 去我的
-		tomy(){
+		tomy() {
 			uni.navigateTo({
-			    url: '/pages/my/my'
+				url: '/pages/my/my'
 			});
 		}
-		}
 	}
+};
 </script>
 
 <style lang="less" scoped>
-	// 导入字体图标
-	@font-face {
-		font-family: 'iconfont';
-		 src: url('~@/static/iconfont.ttf') format('truetype');
+// 导入字体图标
+@font-face {
+	font-family: 'iconfont';
+	src: url('~@/static/iconfont.ttf') format('truetype');
+}
+
+// 设置字体图标
+.iconfont {
+	font-family: 'iconfont' !important;
+	font-size: 2rem;
+	font-style: normal;
+	color: #ff5051;
+}
+.container {
+	width: 100vw;
+	height: 100vh;
+	position: relative;
+	box-sizing: border-box;
+	// background: #f0f0f0;
+
+}
+.top-box {
+	width: 100vw;
+	background: #ffffff;
+	border-radius: 10px;
+	padding: 2rem 1.5rem;
+	box-sizing: border-box;
+	.spansss {
+		margin-right: 1rem;
 	}
-	
-	// 设置字体图标
-	 .iconfont {
-	  font-family: "iconfont" !important;
-	  font-size: 2rem;
-	  font-style: normal;
-	  color: #FF5051;
-	 }
-	.container {
-		width: 100vw;
-		height: 100vh;
-		position: relative;
-		box-sizing: border-box;
-		background: #F0F0F0;
+	.name-box {
+		margin-top: 1rem;
+		font-size: 0.9rem;
+		margin-bottom: 0.5rem;
 	}
-	.top-box{
-		width: 100vw;
-		background: #FFFFFF;
-		border-radius: 10px;
-		padding: 2rem 1.5rem;
-		box-sizing: border-box;
-		.spansss{
-			margin-right: 1rem;
-		}
-		.name-box{
-			margin-top: 1rem;
-			font-size: 0.9rem;
-			margin-bottom: 0.5rem;
-		}
-		.title-box{
-			font-size: 1.1rem;
-			margin-bottom: 1rem;
-		}
-		.intput-box{
-			width: 100%;
-			height: 2.5rem;
-			border-radius: 5px;
-			background-color: #F0F0F0;
-		}
-		.switch-box{
-			display: flex;
-			margin-top:1rem;
-			.switch-item{
-				flex: 1;
-				text-align: center;
-				padding: 0.4rem 0;
-				box-sizing: border-box;
-				
-			}
-			.switch-item-color{
-				color: #FFFFFF;
-				border-radius: 7px;
-				background: red;
-			}
-		}
+	.title-box {
+		font-size: 1.1rem;
+		margin-bottom: 1rem;
 	}
-	// 地图
-	.map-box{
-		width: 100vw;
-		height: 51vh;
-		
-	}
-	
-	.bootm-box{
-		box-sizing: border-box;
-		padding: 2rem 0;
-		background: #FFFFFF;
-		border-radius: 10px;
+	.intput-box {
 		width: 100%;
+		height: 2.5rem;
+		border-radius: 5px;
+		background-color: #f0f0f0;
+	}
+	.switch-box {
 		display: flex;
-		position: fixed;
-		z-index: 100;
-		bottom: 0px;
-		left: 0px;
-		.b-r{
-			border-right: 2px solid #C0C0C0;
-		}
-		view {
+		margin-top: 1rem;
+		.switch-item {
 			flex: 1;
 			text-align: center;
-			span{
-				border-radius: 5px;
-				color: #FFFFFF;
-				padding: 0.5rem 3rem;
-				background: #FF5051;
-			}
+			padding: 0.4rem 0;
+			box-sizing: border-box;
+		}
+		.switch-item-color {
+			color: #ffffff;
+			border-radius: 7px;
+			background: red;
 		}
 	}
+}
+// 地图
+.map-box {
+	width: 100vw;
+	height: 51vh;
+	
+}
+.diss{
+	font-size: 0.8rem;
+}
+.shishi-box{
+		width: 60vw;
+		height: 2rem;
+		background: #FFFFFF;
+		border-radius: 10px 10px 0px 10px;
+		text-align: center;
+		line-height: 2rem;
+		position: absolute;
+		bottom: 2.7rem;
+		z-index: 10000;
+		left: 20vw;
+	}
+.shishi-boxa{
+	width: 60vw;
+	height: 2rem;
+	background: #FFFFFF;
+	// border-radius: 10px;
+	text-align: center;
+	line-height: 2rem;
+	position: absolute;
+	bottom: 1rem;
+	z-index: 10000;
+	left: 20vw; 
+}
+.bootm-box {
+	box-sizing: border-box;
+	padding: 2rem 0;
+	// background: #ffffff;
+	border-radius: 10px;
+	width: 100%;
+	display: flex;
+	position: fixed;
+	z-index: 100;
+	bottom: 0px;
+	left: 0px;
+	.b-r {
+		border-right: 2px solid #c0c0c0;
+		
+	}
+	view {
+		flex: 1;
+		text-align: center;
+		span {
+			border-radius: 5px;
+			color: #ffffff;
+			padding: 0.5rem 3rem;
+			background: #ff5051;
+		}
+	}
+}
 </style>
---
+++ pages/index/real-time.vue
@@ -0,0 +1,82 @@
+<template>
+
+		<map  style="width: 100vw; height: 100%;" longitude="116.451028" latitude="39.949643" scale="12" :markers="markers" :polyline="polyline"></map>
+
+</template>
+
+<script>
+	import amapFile from '../../static/amap-wx.js'
+	export default {
+		data(){
+			return{
+				markers: [{
+				  iconPath: "../../static/touxiang.png",
+				  id: 0,
+				  latitude: 39.989643,
+				  longitude: 116.481028,
+				  width: 23,
+				  height: 33
+				},{
+				  iconPath: "../../static/touxiang.png",
+				  id: 0,
+				  latitude: 39.90816,
+				  longitude: 116.434446,
+				  width: 24,
+				  height: 34
+				}],
+				distance: '',
+				cost: '',
+				polyline: []
+			}
+		  },
+		  mounted() {
+
+		    var _this = this
+			var myAmapFun = new amapFile.AMapWX({key:'d543ccedafcc74cd880a88bfbc0b2576'});
+			
+			myAmapFun.getDrivingRoute({
+				  origin: '116.481028,39.989643',
+				  destination: '116.434446,39.90816',
+				  success: function(data){
+					  var points = [];
+					   if(data.paths && data.paths[0] && data.paths[0].steps){
+							var steps = data.paths[0].steps;
+					        for(var i = 0; i < steps.length; i++){
+								var poLen = steps[i].polyline.split(';');
+								for(var j = 0;j < poLen.length; j++){
+									points.push({
+										  longitude: parseFloat(poLen[j].split(',')[0]),
+										  latitude: parseFloat(poLen[j].split(',')[1])
+										})
+								} 
+							}
+					    }
+						console.log(points)
+						_this.polyline= [{
+									points: points,
+									color: "#ED1C24",
+									width: 10
+						}]
+						if(data.paths[0] && data.paths[0].distance){
+							_this.distance= data.paths[0].distance + '米'
+							console.log(_this.distance)
+						       
+						}
+						if(data.taxi_cost){
+								_this.cost= '打车约' + parseInt(data.taxi_cost) + '元'
+							console.log(_this.cost)
+						}
+						
+								 
+				  }
+			})
+			     
+		  },
+	}
+</script>
+
+<style lang="less" scoped>
+	.a{
+		color: #ED1C24;
+	}
+</style>
